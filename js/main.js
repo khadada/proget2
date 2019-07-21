067094afaf1d9bd35cbdf6a139843bf85423154d
khadada <lkhadada@gmail.com>
2019-07-21 17:45:05 +0100

    all done my website

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var menuToggle = document.querySelector('.fa-bars');
+var menu = document.querySelector('.toggled-menu');
+
+menuToggle.onclick = function () {
+    menu.classList.toggle('slide')
+}
+
+
+
+console.log(menu)
